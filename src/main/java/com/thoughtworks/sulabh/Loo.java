4ea8c93d6da4945cf7a71367636bb6c416c01cca
Aniket Survase <aniketsurvase@INAniketsr.local>
2014-04-15 17:04:14 +0530

    002: Manali: Extracted method in loo class for checking two positions are same.

--- src/main/java/com/thoughtworks/sulabh/Loo.java
+++ src/main/java/com/thoughtworks/sulabh/Loo.java
@@ -3,52 +3,62 @@ package com.thoughtworks.sulabh;
 import com.google.android.gms.maps.model.LatLng;
 
 public class Loo {
-	private String _id;
-	private String name;
-	private double coordinates[];
-	private String operational;
-	private String hygienic;
-	private String paid;
-	private String kind;
-	private String compatibility;
+  private String _id;
+  private String name;
+  private double coordinates[];
+  private String operational;
+  private String hygienic;
+  private String paid;
+  private String kind;
+  private String compatibility;
 
-	private Loo() {
-	}
+  private Loo() {
+  }
 
-	public Loo(String name, LatLng latLng) {
-		this.name = name;
-		this.coordinates = new double[]{latLng.latitude, latLng.longitude};
-	}
+  public Loo(String name, LatLng latLng) {
+    this.name = name;
+    this.coordinates = new double[]{latLng.latitude, latLng.longitude};
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public double[] getCoordinates() {
-		return coordinates;
-	}
+  public double[] getCoordinates() {
+    return coordinates;
+  }
 
-	public String getOperational() {
-		return operational;
-	}
+  public String getOperational() {
+    return operational;
+  }
 
-	public String getHygienic() {
-		return hygienic;
-	}
+  public String getHygienic() {
+    return hygienic;
+  }
 
-	public String getPaid() {
-		return paid;
-	}
+  public String getPaid() {
+    return paid;
+  }
 
-	public String getKind() {
-		return kind;
-	}
+  public String getKind() {
+    return kind;
+  }
 
-	public String getCompatibility() {
-		return compatibility;
-	}
+  public String getCompatibility() {
+    return compatibility;
+  }
 
-	public String get_id() {
-		return _id;
-	}
+  public String get_id() {
+    return _id;
+  }
+
+  public boolean isSamePositionAs(LatLng position) {
+    return areDoublesEqual(this.getCoordinates()[0], position.latitude) && areDoublesEqual(this.getCoordinates()[1], position.longitude);
+  }
+
+  private boolean areDoublesEqual(double a, double b) {
+    double delta = 0.0000005;
+    return Math.abs(a - b) < delta;
+  }
+
 }
