d1352198c43c2b8c2b855221bc076fff6a09f431
Sumit Varude <sumitvarude@gmail.com>
2014-04-29 14:30:38 +0530

    T15: Sumit : removed debug statements

--- src/main/java/com/thoughtworks/sulabh/activity/DetailsActivity.java
+++ src/main/java/com/thoughtworks/sulabh/activity/DetailsActivity.java
@@ -15,59 +15,57 @@ import com.thoughtworks.sulabh.Loo;
 
 public class DetailsActivity extends Activity{
 
-	private Loo loo;
+    private Loo loo;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.details);
-		Intent intent = getIntent();
-		Bundle extras = intent.getExtras();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
 
-		loo = (Loo) extras.getSerializable("Loo");
+        loo = (Loo) extras.getSerializable("Loo");
 
-		String name = loo.getName();
-		TextView placeName = (TextView) findViewById(R.id.placeName);
-		placeName.setText(name);
+        String name = loo.getName();
+        TextView placeName = (TextView) findViewById(R.id.placeName);
+        placeName.setText(name);
 
-		float rating = loo.getRating();
-		RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-		ratingBar.setRating(rating);
+        float rating = loo.getRating();
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingBar.setRating(rating);
 
-		String isOperational = String.valueOf(loo.getOperational());
-		TextView operational = (TextView) findViewById(R.id.isOperational);
-		operational.setText(mapValues(isOperational));
+        String isOperational = String.valueOf(loo.getOperational());
+        TextView operational = (TextView) findViewById(R.id.isOperational);
+        operational.setText(mapValues(isOperational));
 
-		String isHygienic = String.valueOf(loo.getHygienic());
-		TextView hygienic = (TextView) findViewById(R.id.isHygienic);
-		hygienic.setText(mapValues(isHygienic));
+        String isHygienic = String.valueOf(loo.getHygienic());
+        TextView hygienic = (TextView) findViewById(R.id.isHygienic);
+        hygienic.setText(mapValues(isHygienic));
 
-		String isFree = String.valueOf(loo.getFree());
-		TextView free = (TextView) findViewById(R.id.isFree);
-		free.setText(mapValues(isFree));
+        String isFree = String.valueOf(loo.getFree());
+        TextView free = (TextView) findViewById(R.id.isFree);
+        free.setText(mapValues(isFree));
 
-		String ofKind = loo.getType();
-		TextView kind = (TextView) findViewById(R.id.ofKind);
-		kind.setText(ofKind);
+        String ofKind = loo.getType();
+        TextView kind = (TextView) findViewById(R.id.ofKind);
+        kind.setText(ofKind);
 
-		String isSuitableFor = loo.getSuitableFor();
-		TextView suitable = (TextView) findViewById(R.id.suitableFor);
-		suitable.setText(isSuitableFor);
-	}
+        String isSuitableFor = loo.getSuitableFor();
+        TextView suitable = (TextView) findViewById(R.id.suitableFor);
+        suitable.setText(isSuitableFor);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu items for use in the action bar
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.edit, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.edit, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-        System.out.println(item.getTitle() +"***************");
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getTitle().equals("edit")) {
-            System.out.println("********************* " + item.getTitle());
             Intent intent = new Intent(DetailsActivity.this, UpdateLooActivity.class);
             intent.putExtra("Loo", loo);
             startActivity(intent);
@@ -75,18 +73,16 @@ public class DetailsActivity extends Activity{
             return true;
         }
 
-        System.out.println("********************* " + item.getTitle());
         Intent intent = new Intent(DetailsActivity.this,RatingBarView.class);
         startActivity(intent);
         return true;
+    }
 
-	}
-
-	public String mapValues(String field) {
-		if(field.equals("true"))
-			field = "Yes";
-		else
-			field = "No";
-		return field;
-	}
+    public String mapValues(String field) {
+        if(field.equals("true"))
+            field = "Yes";
+        else
+            field = "No";
+        return field;
+    }
 }
